2082bc0ebb283e0d59632725ff60e89edacc9e85
Eli Pandolfo <epandolf@ucsc.edu>
2019-11-08 13:57:42 -0800

    updates

--- src/create_resistance_kmer_class_map.py
+++ src/create_resistance_kmer_class_map.py
@@ -1,10 +1,12 @@
 from large_file_processor import main, write_list, parse_args, load_pickle
 
-def process(data, cim, kim, outfile):
+def process(data, cim, outfile):
+    kim_file = 'data/preprocess/kmer_int_map.pickle'
+    kim = load_pickle(kim_file)
     rkc_chunk = []
     for line in data:
         linelist = line.split()
-        lc = len(cim)
+        lc = int(len(cim) / 2)
         rkc_line = [f'{kim[linelist[0]]} {c}' for c in range(lc)]
         rkc_chunk.append('\n'.join(rkc_line))
     write_list(rkc_chunk, outfile)
@@ -14,10 +16,8 @@ def main_wrapper():
     INPUT_FILE = 'data/preprocess/kmer_sample_map_reduced.txt'
     cim_file = 'data/preprocess/class_int_map.pickle'
     cim = load_pickle(cim_file)
-    kim_file = 'data/preprocess/kmer_int_map.pickle'
-    kim = load_pickle(kim_file)
     outfile = 'data/psl/resistance_kmer_class.txt'
-    main(process, NUM_WORKERS, INPUT_FILE, cim=cim, kim=kim, outfile=outfile)
+    main(process, NUM_WORKERS, INPUT_FILE, cim=cim, outfile=outfile)
 
 if __name__ == '__main__':
     parse_args()
