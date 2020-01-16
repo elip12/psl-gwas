9551725947f184010f35b0719171f3d093609da9
Eli Pandolfo <epandolf@ucsc.edu>
2020-01-16 08:59:24 -0800

    changes to predict_pheno

--- src/predict-pheno/predict_pheno.py
+++ src/predict-pheno/predict_pheno.py
@@ -1,41 +1,57 @@
 import numpy as np
 from sklearn.svm import LinearSVC
 from sklearn.metrics import accuracy_score, precision_recall_fscore_support, confusion_matrix
+from random import shuffle
 
 '''
 convert kmer sample dict into matrix with samples as rows, kmers as columns.
 presence or absence of kmer is value for each cell
 '''
 
-X = np.zeros((300, 9176498))
+size = 15000000
+X = np.zeros((300, size))
 y = np.zeros(300)
-test_X = np.zeros((55, 9176498))
+test_X = np.zeros((55, size))
 test_y = np.zeros(55)
+
+samples = list(range(355))
+shuffle(samples)
+train = samples[:300]
+heldout = samples[300:]
 
 # fill matrix
 # lines is list of all lines in file
-with open('../../data/psl/contains_sample_kmer_head.txt', 'r') as f:
+with open('../../data/psl/contains_sample_kmer.txt', 'r') as f:
     lines = f.readlines()
 for l in lines:
     r,c,v = l.split('\t')
-    if int(r) >= 300:
-        test_X[int(r) - 300][int(c)] = float(v)
+    r = int(r)
+    c = int(c)
+    v = float(v)
+    if r in heldout:
+        test_X[heldout.index(r)][c] = v
     else:
-        X[int(r)][int(c)] = float(v)
+        X[train.index(r)][c] = v
 
 # lines is now a list of resistances
 with open('../../data/psl/resistance_sample_class.txt', 'r') as f:
     lines = f.readlines()
-for l in lines[:331]: # this will change
+for l in lines[332:661]:
     r, _, v = l.split('\t')
-    if int(r) >= 300:
-        test_y[int(r) - 300] = float(v)
+    r = int(r)
+    v = float(v)
+    if r in heldout:
+        test_y[heldout.index(r)] = v
     else:
-        y[int(r)] = float(v)
+        y[train.index(r)] = v
 
 
 svm = LinearSVC()
 svm.fit(X,y)
 predicted = svm.predict(test_X)
-a = accuracy_score(test_y, predicted)
-print(a)
+print(accuracy_score(test_y, predicted))
+print(precision_recall_fscore_support(test_y, predicted))
+print(confusion_matrix(test_y, predicted))
+print(predicted)
+print()
+print(test_y)
