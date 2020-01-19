73a1e34893ce15e548762b3c30565e426264e741
Eli Pandolfo <epandolf@ucsc.edu>
2020-01-19 12:16:33 -0800

    small update to preprocess

--- bin/preprocess.sh
+++ bin/preprocess.sh
@@ -1,6 +1,6 @@
-clean_kmer_db.py                 create_kmer_db.py                    create_sample_int_map.py          __init__.py              __pycache__
-convert_kmer_db_to_psl_input.py  create_kmer_int_map.py               create_similar_antibiotic_map.py  large_file_processor.py  reduce_input.py
-convert_phenos_to_psl_input.py   create_resistance_kmer_class_map.py  create_similar_sample_map.py      pickle_input.py
+#clean_kmer_db.py                 create_kmer_db.py                    create_sample_int_map.py          __init__.py              __pycache__
+#convert_kmer_db_to_psl_input.py  create_kmer_int_map.py               create_similar_antibiotic_map.py  large_file_processor.py  reduce_input.py
+#convert_phenos_to_psl_input.py   create_resistance_kmer_class_map.py  create_similar_sample_map.py      pickle_input.py
 
 python3 src/reduce_input.py
 python3 src/create_kmer_db.py
