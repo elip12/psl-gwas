3f69bb003c717f513c971ee7d7e15a55d7cfdc0c
Eli Pandolfo <epandolf@ucsc.edu>
2020-04-01 19:32:43 -0700

    update count kmers to cat files itself

--- psl-gwas/count_kmers.py
+++ psl-gwas/count_kmers.py
@@ -1,8 +1,26 @@
 from utility import process_file, write_dict, parse_args, printd, \
-check_outfile, get_params
+check_outfile, get_params, write_list
 from multiprocessing import Queue, Manager
 from collections import Counter
+from os import remove
+from os.path import join
 
+# cat all samples together
+def cat_samples(samples, outfile):
+    output_lines = []
+    with open(samples, 'r') as f:
+        lines = f.readlines()
+    
+    for line in lines:
+        sample, fname = line.rstrip().split('\t')
+        if not (fname.endswith('.fa') or fname.endswith('.fsa')):
+            continue
+        with open(fname, 'r') as f:
+            write_list(f.readlines(), outfile)
+   return seqs 
+
+# takes in a chunk of contigs, and creates a counter holding all the kmers
+# and their counts in that chunk
 def process(data, q, k):
     counter = Counter()
     for line in data:
@@ -16,24 +34,24 @@ def process(data, q, k):
 def main():
     params = get_params()
     project = params['project']
-    # num processes in the pool
-    NUM_WORKERS = params['threads']
+
+    samples_file = join(project, 'data', 'raw', params['samples'])
 
     # length of kmer
     k = params['k']
 
     # input data: all input fasta files concatendated in any order
-    INPUT_FILE = f'{project}/data/intermediate/samples.fa'
+    catted_samples = join(project, 'data', 'preprocessed', 'samples.fa')
+    check_outfile(catted_samples)
+    cat_samples(samples_file, catted_samples)
     # output data: a file containing all kmers in the population and their counts
-    outfile = f'{project}/data/intermediate/unique_kmers.txt'
+    outfile = join(project, 'data', 'preprocessed', 'unique_kmers.txt')
     check_outfile(outfile)
 
     # multiprocessing queue for transferring data to the main thread
     q = Manager().Queue()
 
-    # chunkify INPUT_FILE into NUM_WORKERS parts, create MP Pool,
-    # and have each thread run process() on the chunk, with kwargs
-    process_file(process, NUM_WORKERS, INPUT_FILE,
+    process_file(process, catted_samples,
         q=q, k=k)
     
     counter = Counter()
@@ -43,6 +61,9 @@ def main():
    
     # write counter to file
     write_dict(counter, outfile, sep='\t')
+
+    # remove catted samples file
+    remove(catted_samples)
 
 if __name__ == '__main__':
     parse_args()
