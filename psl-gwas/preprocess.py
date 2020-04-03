a32eb3d92ce24cd36e60e39575fe0dba8126e10a
Eli Pandolfo <epandolf@ucsc.edu>
2020-04-03 09:40:07 -0700

    rename files

--- psl-gwas/preprocess.py
+++ psl-gwas/preprocess.py
@@ -1,76 +1,204 @@
-#!/usr/bin/env python3
-from utility import process_file, parse_args, printd, \
-file_exists, get_params, load_pickle, write_list
-from multiprocessing import Queue, Manager
-from collections import Counter
-from preprocess_helpers import num_samples, create_unitig_sample_map, \
-parse_input, similar_sample, create_disp_nodisp_dfs
-import int_maps
-from os.path import join
+#! usr/bin/env python3
+from random import Random
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
-def main():
-    # get params
-    params = get_params()
-    project = params['project']
+# global complement map, only needs to be created once
+COMPLEMENT_MAP = str.maketrans('ATCG', 'TAGC')
 
-    # define file paths
-    unique_kmers_file = join(project, 'data', 'preprocessed', 'unique_kmers.txt')
-    phenos_file = join(project, 'data', 'raw', params['phenos'])
-    samples_file = join(project, 'data', 'raw', params['samples'])
-    similarities_tsv = join(project, 'data', 'preprocessed', 'sample_similarities.tsv')
-    hist_orig_file = join(project, 'data', 'preprocessed', 'hist_orig.png')
-    hist_scaled_file = join(project, 'data', 'preprocessed', 'hist_scaled.png')
-    similar_sample_file = join(project, 'data', 'preprocessed', 'similar_sample_sample.txt')
-    unitigs_file = join(project, 'data', 'preprocessed', 'unitig_sample_map.txt')
-    sim_file = join(project, 'data', 'preprocessed', 'sample_int_map.pkl') 
-    pim_file = join(project, 'data', 'preprocessed', 'pheno_int_map.pkl')
-    uim_file = join(project, 'data', 'preprocessed', 'unitig_int_map.pkl')
+def kmer_frequency_fails(val, upper, lower):
+    if lower <= int(val) <= upper:
+        return False
+    return True
 
-    # create and load sample and pheno int maps
-    if not file_exists(sim_file):
-        int_maps.create_sample_int_map(samples_file, sim_file)
-    if not file_exists(pim_file):
-        int_maps.create_pheno_int_map(phenos_file, pim_file)
-    sim = load_pickle(sim_file)
+# for small files
+def num_samples(fname):
+    with open(fname, 'r') as f:                                                
+        return len(f.readlines()) - 1
+
+def parse_input(infile):
+    seqs = {}
+    with open(infile, 'r') as f:
+        lines = f.readlines()
     
-    if not file_exists(unitigs_file) or not file_exists(similar_sample_file):
-        # dfs holding samples that display vs not display pheno
-        dfdisp, dfnodisp = create_disp_nodisp_dfs(phenos_file)
-        # read in all sequences in input into python object
-        seqs = parse_input(samples_file)
-        # number of samples
-        n_samples = num_samples(samples_file)
-        # upper and lower bounds for frequency of samples to filter kmers by
-        upper = int(params['upperfreq'] * n_samples)
-        lower = int(params['lowerfreq'] * n_samples)
-        # multiprocessing queue for transferring data to the main thread
-        q = Manager().Queue()
-        process_file(create_unitig_sample_map, unique_kmers_file,
-            raw=seqs, q=q, k=params['k'], thresh=params['thresh'], upper=upper,
-            lower=lower, dfdisp=dfdisp, dfnodisp=dfnodisp, sim=sim, n=n_samples)
-       
-        sample_matrix = np.zeros((n_samples, n_samples))
-        num_kmers = 0
-        # write all chunks to output file sequentially
-        create_unitigs_file = False
-        if not file_exists(unitigs_file):
-            create_unitigs_file = True
-        while not q.empty():
-            unitigs, q_num_kmers, q_sample_matrix = q.get()
-            if create_unitigs_file:
-                write_list(unitigs, unitigs_file)
-            num_kmers += q_num_kmers
-            sample_matrix += q_sample_matrix
+    for line in lines:
+        sample, fname = line.rstrip().split('\t')
+        if not fname.endswith('.fa'):
+            continue
+        with open(fname, 'r') as f:
+            contiglines = f.readlines()
+        seqs[sample] = [contigline.rstrip() for contigline in contiglines \
+            if contigline[0] in ['A','T','C','G']]
+    return seqs 
+
+# DNA base complement
+def complement(kmer):
+   return kmer.translate(COMPLEMENT_MAP)
+
+def consolidate(data, k):
+    prev_line = data[0]
+    prev_unitig = prev_line[0]
+    prev_samples = prev_line[1]
+    unitig_db_chunk = []
+    for line in data[1:]:
+        this_unitig = line[0]
+        this_samples = line[1]
+
+        # if kmers are sequential and the same set of samples contain both kmers
+        if prev_unitig[-(k - 1):] == this_unitig[0:k - 1] \
+                and len(this_samples) == len(prev_samples) \
+                and set(this_samples) == set(prev_samples):
+            this_unitig = f'{prev_unitig}{this_unitig[-1]}'
+            prev_line = (this_unitig, prev_line[1])
+        else:
+            unitig_db_chunk.append(prev_line)
+            prev_line = line
         
-        if not file_exists(similar_sample_file):
-            similar_sample(sample_matrix, num_kmers, similarities_tsv,
-                hist_orig_file, hist_scaled_file, similar_sample_file)
-    
-    if not file_exists(uim_file):
-        int_maps.create_unitig_int_map(unitigs_file, uim_file)
+        prev_unitig = this_unitig
+        prev_samples = this_samples
+    return unitig_db_chunk
 
-if __name__ == '__main__':
-    parse_args()
-    main()
+def filter_unitigs(data, thresh, dfdisp, dfnodisp, prop=0.05):
+    kmer_db_chunk = []
+    nphenos = dfdisp.shape[1]
+    for linelist in data:
+        kmer_db_line = [linelist[0]]
+        disp = np.zeros(nphenos)
+        nodisp = np.zeros(nphenos)
+
+        for sample_id, _ in linelist[1]:
+            if sample_id not in dfdisp.index:
+                continue
+            kmer_db_line.append(sample_id)
+            # collect resistant/vulnerable frequencies for each antibiotic for
+            # this kmer
+            disp += dfdisp.loc[sample_id].to_numpy()
+            nodisp += dfnodisp.loc[sample_id].to_numpy()
+
+        # 1 test per antibiotic; kmer needs to pass only 1 to avoid
+        # getting filtered out
+        #a = np.where((disp + nodisp >= thresh) \
+        #            & (disp > 0) \
+        #            & (nodisp / (disp + 0.0001) < prop))[0]
+        #if a.size > 0:
+        kmer_db_chunk.append('\t'.join(kmer_db_line))
+    return kmer_db_chunk
+
+# data is just values in dict
+def sample_kmers(data, sim, n, seed=65665):
+    sample_matrix = np.zeros((n, n)) 
+    num_kmers = int(len(data) * 1.0) # 0.01
+    rng = Random(seed)
+    sampled_kmers = rng.sample(data, num_kmers)
+
+    for line in sampled_kmers:
+        for i, s1_ in enumerate(line[:-1]):
+            s1 = s1_[0]
+            for s2_ in line[i + 1:]:
+                s2 = s2_[0]
+                sample_matrix[int(sim[s1])][int(sim[s2])] += 1 
+                sample_matrix[int(sim[s2])][int(sim[s1])] += 1
+    return num_kmers, sample_matrix
+
+# Creates a dictionary of all kmers passed in data, and their complements
+# Then, iterates through genomes. If a genome contains a kmer, that genome's
+# metadata is added to dict entry for that kmer.
+# After all samples have been iterated over, writes kmers to file.
+def create_unitig_sample_map(data, raw, k, q, upper, lower, thresh,
+        dfdisp, dfnodisp, sim, n):
+   
+    # get all kmers in chunk and complement them
+    kmers = {}
+    for line in data:
+        kmer, count = line.split('\t')
+        if kmer_frequency_fails(count, upper, lower):
+            continue
+        comp = complement(kmer)
+        kmers[kmer] = []
+        kmers[comp] = []
+    
+    # map all kmers in chunk to samples containing them
+    for count, (raw_id, seq) in enumerate(raw.items()):
+        for c_id, contig in enumerate(seq):
+            l = len(contig)
+            if l >= k: # ensure this contig is long enough to sample
+                for i in range(l - k + 1):
+                    kmer = contig[i: i + k]
+                    if kmer in kmers:
+                        kmers[kmer].append((raw_id, c_id))
+    kmers = {k:v for k,v in kmers.items() if len(v) > 0} 
+    num_kmers, sample_matrix = sample_kmers(list(kmers.values()), sim, n)
+    unitigs = consolidate(list(kmers.items()), k)
+    unitigs = filter_unitigs(unitigs, thresh, dfdisp, dfnodisp)
+    q.put((unitigs, num_kmers, sample_matrix))
+
+    ## combine similarity matrices from each thread into a single matrix,
+    ## and get total number of kmers sampled
+def similar_sample(sample_matrix, num_kmers, similarities_tsv,
+        hist_orig_file, hist_scaled_file, outfile):
+    np.fill_diagonal(sample_matrix, np.nan) 
+    sample_matrix = np.triu(sample_matrix)
+
+    # scale similarity counts to values in [0, 1]
+    sample_matrix /= num_kmers
+
+    df = pd.DataFrame(sample_matrix)
+
+    # dump to tsv file for ease of restoring, and because tsv file of similarities
+    # is a common input to other mGWAS programs
+    df.to_csv(similarities_tsv, sep='\t')
+    
+    # optionally read csv for ease of restoring
+    # df = pd.read_csv('data/intermediate/similarities.tsv', sep='\t', index_col=0)
+   
+    # create similarity histogram and save it
+    plt.hist(df.values, facecolor='green')
+    plt.savefig(hist_orig_file, dpi=150)
+
+    df = df.stack()
+    df = df.reset_index()
+    
+    # set threshold; 0.75 means drop lowest 75%, keep highest 25%
+#    thresh = 0.9
+#    # find numeric cutoff; the lowest 75% of the data are below this value
+#    cutoff = df[0].quantile(thresh)
+#    # cut off all values below (less similar than) cutoff
+#    df = df[df[0] > cutoff]
+#    # determine new min, max, range
+#    min_ = df[0].min()
+#    max_ = df[0].max()
+#    range_ = max_ - min_
+#    # shift df left by the min so the new min is 0
+#    df[0] -= min_
+#    # rescale data to [0,0.5]
+#    df[0] /= range_ * 2
+#    # shift right by 0.5 so the new range is [0.5, 1]
+#    df[0] += 0.5
+
+    # create similarity histogram and save it
+    plt.hist(df[0], facecolor='green')
+    plt.savefig(hist_scaled_file, dpi=150)
+
+    # write to csv 
+    df.to_csv(outfile, sep='\t', index=False, header=False)
+
+def convert_to_binary(x):
+    if x > 1.0:
+        return 1.0
+    if x < 0.0:
+        return 0.0
+    return x
+
+# separate phenos df into 2 dfs, one holding phenos present and 1 holding
+# phenos absent.
+def create_disp_nodisp_dfs(phenos):
+    df = pd.read_csv(phenos, delimiter='\t')
+    idcol = df.columns[0]
+    df.set_index(idcol, inplace=True)
+    
+    df = df.applymap(convert_to_binary)
+    dfdisp = df.fillna(0)
+    dfnodisp = 1 - df.fillna(1)
+    return dfdisp, dfnodisp
 
--- psl-gwas/pslprep_main.py
+++ psl-gwas/pslprep_main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from utility import process_file, write_list, parse_args, load_pickle, printd, \
 get_params, file_exists
-from psl_input_helpers import unitig_db, sample_pheno, similar_pheno
+from pslprep import unitig_db, sample_pheno, similar_pheno
 from multiprocessing import Manager, Queue
 from os.path import join
 
