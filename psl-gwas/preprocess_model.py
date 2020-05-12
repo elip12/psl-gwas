395589508429c84c7c4561ff764caafcc3bcd298
Eli Pandolfo <epandolf@ucsc.edu>
2020-05-12 13:00:52 -0700

    change blocking on sample similarity to be more strict for similar, and use all pairs for dissimilar

--- psl-gwas/preprocess_model.py
+++ psl-gwas/preprocess_model.py
@@ -223,13 +223,13 @@ def similar_sample(sample_matrix, num_kmers, similarities_tsv,
     df = df.reset_index()
     df = df[df[0] > 0] # remove the lower half of the triangle
     # set threshold; 0.75 means drop lowest 75%, keep highest 25%
-    thresh = 0.95
+    thresh = 0.98
     # find numeric cutoff; the lowest 75% of the data are below this value
     highcutoff = df[0].quantile(thresh)
-    lowcutoff = df[0].quantile(1 - thresh)
+    #lowcutoff = df[0].quantile(1 - thresh)
     # cut off all everything in the middle; only keep the very similar and very dissimilar
     simdf = df[df[0] >= highcutoff].copy(deep=True)
-    dissimdf = df[df[0] <= lowcutoff].copy(deep=True)
+    dissimdf = df #[df[0] <= lowcutoff].copy(deep=True)
     dissimdf[0] = 1 - dissimdf[0]
     dfs = (simdf, dissimdf)
     # determine new min, max, range
@@ -239,12 +239,18 @@ def similar_sample(sample_matrix, num_kmers, similarities_tsv,
         min_ = df[0].min()
         max_ = df[0].max()
         range_ = max_ - min_
-        # shift df left by the min so the new min is 0.0001
+        # shift df left by the min so the new min is 0
         df[0] -= min_
-        # rescale data to [0,0.5]
-        df[0] /= range_ * 2
-        # rescale data to [0.5, 1]
-        df[0] += 0.5
+        # rescale data to [0,0.5] or [0,1]
+        if i == 0:
+            scale_factor = 2
+            intercept = 0.5
+        else:
+            scale_factor = 1
+            intercept = 0.00001
+        df[0] /= range_ * scale_factor
+        # rescale data to [0.5, 1] or [0.00001, 1]
+        df[0] += intercept
         # create similarity histogram and save it
         try:
             plt.hist(df[0], bins=50, facecolor='green')
