ffa0f5b620d8fdf8e906e469a740830f4aa125be
Eli Pandolfo <epandolf@ucsc.edu>
2020-06-08 17:25:08 -0700

    update test startproject

--- tests/test_startproject.py
+++ tests/test_startproject.py
@@ -1,14 +1,24 @@
 import sys
 import subprocess
 import unittest
+import shutil
+from os.path import join
 
 class E2E(unittest.TestCase):
     def test_start_project(self):
-        completed_process = subprocess.run(['./bin/startproject.sh', 'testproject'], input=b'y', stdout=subprocess.DEVNULL)
+        rawsrc = join('tests', 'data', 'raw')
+        rawdst = join('testproject', 'data')
+
+        #completed_process = subprocess.run(['./bin/startproject.sh', 'testproject'], input=b'y', stdout=subprocess.DEVNULL)
+        #self.assertEqual(completed_process.returncode, 0)
+        #subprocess.run(['cp', '-r', rawsrc, rawdst]) 
+        
+
 
     @classmethod
     def tearDownClass(cls):
-        subprocess.run(['rm', '-rf', 'testproject'])
+        pass
+        #subprocess.run(['rm', '-rf', 'testproject'])
 
 if __name__ == '__main__':
     unittest.main()
